36dfc9d03436d6f226308a8380026180521c1415
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-11-17 11:03:00 +0200

    Added routes to the two pages

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,17 @@
  */
 import React from "react"
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./Home.jsx"
+import About from "./About.jsx"
+
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
       </Routes>
     </BrowserRouter>
   )
