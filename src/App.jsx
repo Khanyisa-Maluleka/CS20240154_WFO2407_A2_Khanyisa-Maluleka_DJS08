68b6250a0cb8310f7ed2ae92efe4a61adcfcd1a3
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-11-17 14:29:33 +0200

    Added a Vans page

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,24 @@ import React from "react"
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home.jsx"
 import About from "./pages/About.jsx"
+import Vans from "./pages/Vans.jsx"
 
+//import "./server.js"
 
 function App() {
- 
   return (
     <BrowserRouter>
       <header>
-        <Link to="/">#VANLIFE</Link>
+        <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
-          <Link to = "/about">About</Link>
+          <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   )
